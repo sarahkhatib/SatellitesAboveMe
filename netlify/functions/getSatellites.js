ac43ef268053d1f15942d48c452f477f6149d655
Sarah <sarah.khatib.28@gmail.com>
2024-09-16 03:58:41 +0200

    removing console prints

--- netlify/functions/getSatellites.js
+++ netlify/functions/getSatellites.js
@@ -2,19 +2,15 @@
 const axios = require('axios');
 
 exports.handler = async (event) => {
-  const start = Date.now(); // Start time
   console.log('Function started');
   try {
     const { observerLatitude, observerLongitude, observerAltitude } = JSON.parse(event.body);
     console.log('Parsed request body:', { observerLatitude, observerLongitude, observerAltitude });
     const API_KEY = process.env.VITE_N2YO_API_KEY;
-    const searchRadius = 30;
+    const searchRadius = 90;
     const apiUrl = `https://api.n2yo.com/rest/v1/satellite/above/${observerLatitude}/${observerLongitude}/${observerAltitude || 0}/${searchRadius}/52/&apiKey=${API_KEY}`;
-    console.log('API URL:', apiUrl);
 
     const response = await axios.get(apiUrl);
-    const elapsed = Date.now() - start; // Time taken for API response
-    console.log('API response received in', elapsed, 'ms');
     
     // Ensure response data is valid
     if (response.data && response.data.above) {
